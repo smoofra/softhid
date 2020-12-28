53936c16e42022bfdf0be07b3e6ba7f77e465a21
Lawrence D'Anna <larry@elder-gods.org>
2020-12-27 18:06:15 -0800

    add mac app template

---
+++ app/softhid/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  softhid
+//
+//  Created by Lawrence D'Anna on 12/27/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
